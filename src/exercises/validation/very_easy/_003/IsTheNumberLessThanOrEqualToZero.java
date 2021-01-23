be3a71643dc5e414806c09d80c7ccce75cc1a900
basha <basharbdewi86@gmail.com>
2021-01-23 21:55:00 +0100

    It is error at type else if , it is enough with else.

---
+++ src/exercises/validation/very_easy/_003/IsTheNumberLessThanOrEqualToZero.java
@@ -0,0 +1,32 @@
+package exercises.validation.very_easy._003;
+
+/**
+ *
+ *Is the Number Less than or Equal to Zero?
+ * Create a method that takes an integer as its only argument and returns true if it's less than or equal to zero,
+ * otherwise return false.
+ *
+ * Examples
+ * lessThanOrEqualToZero(5) ➞ false
+ *
+ * lessThanOrEqualToZero(0) ➞ true
+ *
+ * lessThanOrEqualToZero(-2) ➞ true
+ * Notes
+ * Don't forget to return the result.
+ * If you get stuck on a challenge, find help in the Resources tab.
+ * If you're really stuck, unlock solutions in the Solutions tab.
+ */
+public class IsTheNumberLessThanOrEqualToZero {
+    public static void main(String[]args)
+    {
+        System.out.println(lessThanOrEqualToZero(5));
+    }
+    public static boolean lessThanOrEqualToZero(int a)
+    {
+        if (a==0 || a>0){
+            return true;}
+        else {
+            return false;}
+    }
+}
