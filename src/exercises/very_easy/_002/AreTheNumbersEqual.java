aee495f3bf39eefd1b846d5d3207993fe1fb6567
basha <basharbdewi86@gmail.com>
2021-01-23 21:06:35 +0100

    Adding the exercise description.

--- src/exercises/very_easy/_002/AreTheNumbersEqual.java
+++ src/exercises/very_easy/_002/AreTheNumbersEqual.java
@@ -1,5 +1,19 @@
 package exercises.very_easy._002;
 
+/**
+ * Are the Numbers Equal?
+ * Create a function that returns true when x is equal to y; otherwise return false.
+ *
+ * Examples
+ * isSameNum(4, 8) ➞ false
+ *
+ * isSameNum(2, 2) ➞  true
+ *
+ * isSameNum(42, 32) ➞  false
+ * Notes
+ * Don't forget to return the result.
+ */
+
 public class AreTheNumbersEqual {
     public static void main(String[] args) {
 
