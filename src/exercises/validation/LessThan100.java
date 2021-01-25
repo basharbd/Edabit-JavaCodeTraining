ec8fa15eea5528e5e4eb77b5b5471d1099f6ced0
basha <basharbdewi86@gmail.com>
2021-01-25 23:38:56 +0100

    Add the comment

--- src/exercises/validation/LessThan100.java
+++ src/exercises/validation/LessThan100.java
@@ -1,8 +1,21 @@
 package exercises.validation;
 
+/**
+ * Less Than 100?
+ * Given two numbers, return true if the sum of both numbers is less than 100. Otherwise return false.
+ *
+ * Examples
+ * lessThan100(22, 15) ➞ true
+ * // 22 + 15 = 37
+ *
+ * lessThan100(83, 34) ➞ false
+ * // 83 + 34 = 117
+ *
+ * lessThan100(3, 77) ➞ true
+ */
 public class LessThan100 {
     public static void main(String[] args){
-        lessThan100(88,55);
+        System.out.println(lessThan100(88,55));
 
     }
     public static boolean lessThan100(int a, int b){
