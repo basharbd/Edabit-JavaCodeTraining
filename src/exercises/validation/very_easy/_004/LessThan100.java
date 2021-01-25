f383b2c265003c4648c1213af0148efbaa184713
basha <basharbdewi86@gmail.com>
2021-01-25 23:23:10 +0100

    Return nothing

---
+++ src/exercises/validation/very_easy/_004/LessThan100.java
@@ -0,0 +1,12 @@
+package exercises.validation.very_easy._004;
+
+public class LessThan100 {
+    public static void main(String[] args){
+        lessThan100(88,55);
+
+    }
+    public static boolean lessThan100(int a, int b){
+       return a+b<100;
+    }
+
+}
