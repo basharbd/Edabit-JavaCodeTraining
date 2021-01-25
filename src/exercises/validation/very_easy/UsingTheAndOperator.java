d49d3991b8d0051f020f421c435a6e691b550c3e
basha <basharbdewi86@gmail.com>
2021-01-26 00:49:46 +0100

    Done

---
+++ src/exercises/validation/very_easy/UsingTheAndOperator.java
@@ -0,0 +1,34 @@
+package exercises.validation.very_easy;
+
+/**
+ * Using the "&&" Operator
+ * Java has a logical operator &&. The && operator takes two boolean values, and returns true if both values are true.
+ *
+ * Consider a && b:
+ *
+ * a is checked if it is true or false.
+ * If a is false, false is returned.
+ * b is checked if it is true or false.
+ * If b is false, false is returned.
+ * Otherwise, true is returned (as both a and b are therefore true ).
+ * The && operator will only return true for true && true.
+ *
+ * Make a function using the && operator.
+ *
+ * Examples
+ * and(true, false) ➞ false
+ *
+ * and(true, true) ➞ true
+ *
+ * and(false, true) ➞ false
+ *
+ * and(false, false) ➞ false
+ */
+public class UsingTheAndOperator {
+    public static void main(String[] args){
+        System.out.println(and(false,true));
+    }
+    public static boolean and(boolean a,boolean b){
+        return a&&b==true;
+    }
+}
